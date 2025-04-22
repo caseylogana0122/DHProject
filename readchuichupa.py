574b308ba21cd6b2a06cef3e5e6e09836c57c702
caseylogana0122 <108601919+caseylogana0122@users.noreply.github.com>
2025-04-21 23:29:03 -0400

    Update readchuichupa.py

--- readchuichupa.py
+++ readchuichupa.py
@@ -15,6 +15,13 @@ print(female_count)
 male_count = df_chu['sex_indicated'].str.lower().value_counts().get('male', 0)
 print(male_count)
 
+#Count how mnay times 'female' appears in 'sex_indicated' AND 'married' appears in 'marriage_status'
+female_married_count = df_chu[
+    (df_dublan['sex_indicated'].str.lower() == 'female') &
+    (df_dublan['marriage_status'].str.lower() == 'married')
+].shape[0]
+print(female_married_count)
+
 # Count how many times 'female' appears in 'sex_indicated' AND 'single' appears in 'marriage_status'
 female_single_count = df_chu[
     (df_chu['sex_indicated'].str.lower() == 'female') &
@@ -22,7 +29,45 @@ female_single_count = df_chu[
 ].shape[0]
 print(female_single_count)
 
-y = np.array([female_count, male_count])
-mylabels = ['Females', 'Males']
-plt.pie(y, labels=mylabels)
+# Count how many times 'male' appears in 'sex_indicated' AND 'single' appears in 'marriage_status'
+male_single_count = df_chu[
+    (df_chu['sex_indicated'].str.lower() == 'male') &
+    (df_chu['marriage_status'].str.lower() == 'single')
+].shape[0]
+print(male_single_count)
+
+# Count how many times 'male' appears in 'sex_indicated' AND 'married' appears in 'marriage_status'
+male_married_count = df_chu[
+    (df_chu['sex_indicated'].str.lower() == 'male') &
+    (df_chu['marriage_status'].str.lower() == 'married')
+].shape[0]
+print(male_married_count)
+
+#Creating Pie Chart with Total Gender Distrubution in Chuichupa Colony
+y = np.array([female_count, male_count,])
+mylabels = ['Females', 'Males',]
+colors = ['pink', 'blue']
+
+total = sum(y)
+def autopct_format(pct):
+    absolute = int(pct/100.*total)
+    return "{:.1f}%\n({:d})".format(pct, absolute)
+
+plt.title("Total Gender Distribution in Colony Chuichupa")
+plt.pie(y, labels=mylabels, colors=colors, autopct=autopct_format)
+plt.show()
+
+#Creating Pie Chart with Married to Single Gender Distrubution in Chuichupa Colony
+y = np.array([female_married_count, male_married_count,male_single_count,female_single_count])
+mylabels = ['Married Females', 'Married Males' , 'Single Males', 'Single Females']
+myexplode = (0,0,0.1,0.1)  # explode the 3rd and 4th slice
+colors = ['pink', 'blue', 'green', 'orange']
+
+total = sum(y)
+def autopct_format(pct):
+    absolute = int(pct/100.*total)
+    return "{:.1f}%\n({:d})".format(pct, absolute)
+
+plt.title("Married-to-Single Distribution in Colony Chuichupa")
+plt.pie(y, labels=mylabels, explode=myexplode, colors=colors, autopct=autopct_format)
 plt.show()
